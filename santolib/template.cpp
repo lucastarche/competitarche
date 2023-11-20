17b66edba2828ce691ee7eca2412c18bbac3099a
tarche <lucastarche@gmail.com>
2023-11-19 21:00:19 -0300

    Initial commit

---
+++ santolib/template.cpp
@@ -0,0 +1,17 @@
+/*
+ * Problem:    $(PROBLEM)
+ * Judge:      $(JUDGE)
+ * Contest:    $(CONTEST)
+ * URL:        $(URL)
+ * Solve date: $(DATE)
+ * Tags:
+ */
+#include "santolib/template.hpp"
+
+int main() {
+  fastIO();
+
+  tint t;
+  cin >> t;
+  forsn(testcase, 1, t + 1) {}
+}
